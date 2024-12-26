36bda6e0694933f767156b2b39515697f44f9f3d
Prasad Pathare <prasadsachinist10@gmail.com>
2024-12-26 14:21:35 +0000

    UX + Functionality

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -3,8 +3,6 @@ import Zoom from "react-medium-image-zoom";
 import { imageMap, interiorMap } from "@/lib/imageMap";
 import "react-medium-image-zoom/dist/styles.css";
 
-// exterior: "Black Pearl"
-// wheel: "Slipstream"
 function Preview({ exterior, wheel, interior }) {
   return (
     <>
@@ -22,7 +20,9 @@ function Preview({ exterior, wheel, interior }) {
 
       {/* Exterior Description */}
       <section className="my-4 pl-2">
-        <h3 className="text-2xl font-bold">{imageMap[exterior].finish}</h3>
+        <h3 className="text-2xl font-bold">
+          {exterior}: {imageMap[exterior].finish} w/ {wheel}
+        </h3>
         <p>{imageMap[exterior].description}</p>
       </section>
       {/* Interior Image */}
