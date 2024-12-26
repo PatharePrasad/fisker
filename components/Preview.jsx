f31aecd8faacc1dd6675651c5d82c5c92fbfca73
Prasad Pathare <prasadsachinist10@gmail.com>
2024-12-26 13:54:02 +0000

    Add interiorMap

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import Zoom from "react-medium-image-zoom";
+import { imageMap, interiorMap } from "@/lib/imageMap";
 import "react-medium-image-zoom/dist/styles.css";
 
-function Preview() {
+// exterior: "Black Pearl"
+// wheel: "Slipstream"
+function Preview({ exterior, wheel, interior }) {
   return (
     <>
       {/* Exterior Image */}
       <Zoom>
         <Image
-          src="/slipstream_black.webp"
+          src={imageMap[exterior].wheels[wheel]}
           alt=""
           width={0}
           height={0}
@@ -19,13 +22,13 @@ function Preview() {
 
       {/* Exterior Description */}
       <section className="my-4 pl-2">
-        <h3 className="text-2xl font-bold">Metallic Gloss</h3>
-        <p>A rich and luxurious interior with a modern and sleek design.</p>
+        <h3 className="text-2xl font-bold">{imageMap[exterior].finish}</h3>
+        <p>{imageMap[exterior].description}</p>
       </section>
       {/* Interior Image */}
       <Zoom>
         <Image
-          src="/slipstream_black.webp"
+          src={interiorMap[interior]}
           alt=""
           width={0}
           height={0}
@@ -34,7 +37,7 @@ function Preview() {
         />
       </Zoom>
       <section className="mt-4 pl-2">
-        <h3 className="text-xl font-bold">Interior Color</h3>
+        <h3 className="text-xl font-bold">Interior {interior}</h3>
       </section>
     </>
   );
